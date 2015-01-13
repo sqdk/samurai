a772ff28334490be406b3ef14ff1edf076b51672
sqdk <christian.probst.dk@gmail.com>
2015-01-13 22:06:53 +0100

    Cleaned code

--- samurai.go
+++ samurai.go
@@ -7,15 +7,20 @@ import (
 )
 
 var (
-	spaceDelim = uint8(',')
-	openDelim  = uint8('(')
-	closeDelim = uint8(')')
+	elementDelim = uint8(',')
+	openDelim    = uint8('(')
+	closeDelim   = uint8(')')
+	strict       = false
 )
 
-func SetDelimiters(spaceDelimiter, openDelimiter, closeDelimiter uint8) {
-	spaceDelim = spaceDelimiter
+func SetDelimiters(elementDelimiter, openDelimiter, closeDelimiter uint8) {
+	elementDelim = elementDelimiter
 	openDelim = openDelimiter
 	closeDelim = closeDelimiter
+}
+
+func SetStrict(enabled bool) {
+	strict = enabled
 }
 
 func TokenizeBlock(block string, patternBlock string) map[string]string {
@@ -37,7 +42,13 @@ func TokenizeBlock(block string, patternBlock string) map[string]string {
 			if elements[i] == "nil" {
 				continue
 			}
+
 			if i < len(str) {
+				if i > len(elements) && strict {
+					//log.Printf("%#v - Len %v, \n%#v - Len %v\n", elements, len(elements), str, len(str))
+					//log.Println("More values defined in pattern than in string to tokenize.")
+					return nil
+				}
 				allData[elements[i]] = str[i]
 			}
 		} else {
@@ -73,7 +84,7 @@ func stripLayer(pattern string) (elements []string) {
 				counting = true
 				es = i + 1
 			}
-		} else if (pattern[i] == spaceDelim || pattern[i] == closeDelim) && c == 1 {
+		} else if (pattern[i] == elementDelim || pattern[i] == closeDelim) && c == 1 {
 			elements = append(elements, pattern[es:i])
 			es = i + 1
 		} else if pattern[i] == closeDelim {
@@ -86,7 +97,7 @@ func stripLayer(pattern string) (elements []string) {
 
 //Should check if pattern has same amount of open and close delimiters, and check if all split values are present
 //Could also check if number of keyswords > 0
-func validatePattern(pattern string) error {
+func ValidatePattern(pattern string) error {
 	delimCount := 0
 	for i := 0; i < len(pattern); i++ {
 		if pattern[i] == openDelim {
@@ -95,7 +106,7 @@ func validatePattern(pattern string) error {
 			if i-1 < 0 {
 				return errors.New(fmt.Sprintf("Missing splitter string next to open delimiter (char %v)", 0))
 			}
-			if pattern[i-1] == openDelim || pattern[i-1] == closeDelim || pattern[i-1] == spaceDelim {
+			if pattern[i-1] == openDelim || pattern[i-1] == closeDelim || pattern[i-1] == elementDelim {
 				return errors.New(fmt.Sprintf("Missing splitter string next to open delimiter (char %v)", i))
 			}
 		} else if pattern[i] == closeDelim {
--- samurai_test.go
+++ samurai_test.go
@@ -4,9 +4,47 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"testing"
 	"time"
 )
+
+func TestTokenizeLineAsyncApacheLog(t *testing.T) {
+	exampleWithoutDelimiter := "[( (ip,ident,user),](date,\"(nil, (method,url,httpver), (nil,httpcode,reqsize))))"
+	data := TokenizeBlock("127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326", exampleWithoutDelimiter)
+
+	if data["ip"] != "127.0.0.1" {
+		t.Error("Failed to extract IP correctly")
+	}
+
+	if data["ident"] != "-" {
+		t.Error("Failed to extract ident correctly")
+	}
+
+	if data["date"] != "10/Oct/2000:13:55:36 -0700" {
+		t.Error("Failed to extract date correctly")
+	}
+
+	if data["method"] != "GET" {
+		t.Error("Failed to extract http method correctly")
+	}
+
+	if data["url"] != "/apache_pb.gif" {
+		t.Error("Failed to extract url correctly")
+	}
+
+	if data["httpver"] != "HTTP/1.0" {
+		t.Error("Failed to extract http version correctly")
+	}
+
+	if data["httpcode"] != "200" {
+		t.Error("Failed to extract http code correctly")
+	}
+
+	if data["reqsize"] != "2326" {
+		t.Error("Failed to extract request size correctly")
+	}
+}
 
 func TestTokenizeLineApacheLog(t *testing.T) {
 	exampleWithoutDelimiter := "[( (ip,ident,user),](date,\"(nil, (method,url,httpver), (nil,httpcode,reqsize))))"
@@ -89,7 +127,7 @@ func TestTokenizeLineApacheLogAltPattern(t *testing.T) {
 
 func TestValidatePattern(t *testing.T) {
 	exampleWithoutDelimiter := "[( (ip,nil,user),](date,\"(nil, (method,url,httpver), (nil,httpcode,reqsize))))"
-	err := validatePattern(exampleWithoutDelimiter)
+	err := ValidatePattern(exampleWithoutDelimiter)
 	t.Log(err)
 }
 
@@ -120,6 +158,56 @@ func BenchmarkTokenizeLine(b *testing.B) {
 	}
 }
 
+func BenchmarkRegexTest(b *testing.B) {
+	b.StopTimer()
+	s := "Mr. Leonard Spock"
+	re1, err := regexp.Compile(`(Mr)(s)?\. (\w+) (\w+)`)
+	if err != nil {
+		b.Log(err)
+	}
+	b.StartTimer()
+	for i := 0; i <= b.N; i++ {
+		re1.FindStringSubmatch(s)
+	}
+}
+
+func BenchmarkMapConcat(b *testing.B) {
+	test1 := map[string]string{
+		"test":  "test",
+		"test2": "test2",
+		"test3": "test2",
+		"test4": "test2",
+		"test5": "test2",
+		"test6": "test2",
+		"test7": "test2",
+		"test8": "test2",
+	}
+	test2 := map[string]string{
+		"test":  "test",
+		"test2": "test2",
+		"test3": "test2",
+		"test4": "test2",
+		"test5": "test2",
+		"test6": "test2",
+		"test7": "test2",
+		"test8": "test2",
+	}
+	for i := 0; i <= b.N; i++ {
+		for k, v := range test1 {
+			test2[k] = v
+		}
+	}
+}
+
+func BenchmarkArrayConcat(b *testing.B) {
+	test1 := []string{"test", "test2", "test", "test2", "test", "test2", "test", "test2"}
+	test2 := []string{"test", "test2", "test", "test2", "test", "test2", "test", "test2"}
+
+	for i := 0; i <= b.N; i++ {
+		_ = append(test1, test2...)
+	}
+}
+
 func TestTokenizeLine(t *testing.T) {
 	exampleWithoutDelimiter := " (ip,nil,user,[(nil,date),](tz),\"(nil,method),url,\"(httpver),code,size)"
 	//exampleWithoutDelimiter := "[( (ip,nil,user),](date,\"(nil, (method,url,httpver), (nil,httpcode,reqsize))))"
@@ -130,7 +218,7 @@ func TestTokenizeLine(t *testing.T) {
 	}
 	scanner := bufio.NewScanner(f)
 	timeNow := time.Now()
-	limit := 1000000
+	limit := 100
 	c := 0
 	for scanner.Scan() {
 		if c == limit {
@@ -144,4 +232,13 @@ func TestTokenizeLine(t *testing.T) {
 
 	fmt.Printf("Parsed %v lines in %v\n", c, timeAfter.Sub(timeNow))
 	fmt.Printf("%v ns pr op\n", timeAfter.Sub(timeNow).Nanoseconds()/int64(c))
+}
+
+func TestOddPattern(t *testing.T) {
+	exampleWithoutDelimiter := "[( (ip,nil,user),](date,\"(nil, (method,url,httpver), (nil,httpcode,reqsize))))"
+	testData := "ip114.phx.primenet.com - - [04/Sep/1995:12:13:34 -0400] \"GET /pub/listserv/scroll.gif\" 200 999"
+
+	data := TokenizeBlock(testData, exampleWithoutDelimiter)
+
+	fmt.Println(data)
 }
